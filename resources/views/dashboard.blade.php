8d5edf6fa65f9dbc6ab16d0cb56b74b399620a5e
Deyvid Silva <deyvidsant@hotmail.com>
2024-11-04 08:18:18 -0300

    alteração de cor bg alerta

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <!-- Formulário -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class=" dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-white  flex justify-center">
                     <form action="{{ route('import.excel') }}" method="POST" enctype="multipart/form-data">
                         @csrf
@@ -56,10 +56,11 @@
     </div>
 
     <!-- Alerta com estilização aprimorada -->
-    <div class="bg-blue-100  border-blue-500 text-green-700 p-4 rounded-md my-4 mx-6">
+    <div class="bg-blue-500  border-blue-500 text-green-700 p-4 rounded-md my-4 mx-6">
         <div class="flex justify-center">
             <svg class="w-6 h-6 mr-4 text-red-500" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M9 2a7 7 0 100 14 7 7 0 000-14zM7.707 9.707a1 1 0 01-1.414 0L5.293 8.707a1 1 0 011.414-1.414L7 8.586l3.293-3.293a1 1 0 111.414 1.414L7.707 9.707z"/>
+                <path
+                    d="M9 2a7 7 0 100 14 7 7 0 000-14zM7.707 9.707a1 1 0 01-1.414 0L5.293 8.707a1 1 0 011.414-1.414L7 8.586l3.293-3.293a1 1 0 111.414 1.414L7.707 9.707z"/>
             </svg>
             <div>
                 <p class="font-semibold text-red-600">Importante:</p>
